88bd597dcd82d7add373b0c5ae1c2b7f67ecca70
IvanIvPe <pesicivan2002@gmail.com>
2025-11-26 12:36:33 +0100

    Updates

--- src/App.js
+++ src/App.js
@@ -3,18 +3,18 @@ import BookList from './components/Booklist';
 import ThemeContextProvider from './contexts/ThemeContext';
 import ThemeToggle from './components/ThemeToggle';
 import AuthContextProvider from './contexts/AuthContext';
-import { BookContext } from './contexts/BookContext';
+import BookContextProvider from './contexts/BookContext'; 
 
 function App() {
   return (
     <div className="App">
       <ThemeContextProvider>
         <AuthContextProvider>
-        <Navbar />
-        <BookContext.Provider>
-        <BookList />
-        </BookContext.Provider>
-        <ThemeToggle />
+          <Navbar />
+          <BookContextProvider>
+            <BookList />
+          </BookContextProvider>
+          <ThemeToggle />
         </AuthContextProvider>
       </ThemeContextProvider>
     </div>
